7c56e8eb9ab9061f395c8d6359df2f7ffc7d1c19
kaislazreg <lazreg.kaiss@gmail.com>
2023-01-26 16:24:10 +0100

    maps

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>go my code </h1> 
+
+   <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=gomycode%20lac%201&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+
+      <h1>this is my location</h1>
+    </div>
+  );
+}
+
+export default App;
